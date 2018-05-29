0df53e56e3c55aae1c4f510a27fd8301e8c6c935
sreco002 <sreco002@gmail.com>
2018-05-29 02:28:15 +0100

    add arduino IOT

---
+++ arduino-code/DCBot/DCBot.ino
@@ -0,0 +1,78 @@
+
+/**
+    BasicBot - The basic working bot.
+    Part of the Asimi project - http://sudarmuthu.com/arduino/asimi
+   Copyright 2011  Sudar Muthu  (email : sudar@sudarmuthu.com)
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <sudar@sudarmuthu.com> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer or coffee in return - Sudar
+ * ----------------------------------------------------------------------------
+ */
+
+#define E1 5  // Enable Pin for motor 1
+#define E2 4  // Enable Pin for motor 2
+
+#define I1 3  // Control pin 1 for motor 1
+#define I2 2  // Control pin 2 for motor 1
+#define I3 1  // Control pin 1 for motor 2
+#define I4 0  // Control pin 2 for motor 2
+
+void setup() {
+
+    pinMode(E1, OUTPUT);
+    pinMode(E2, OUTPUT);
+
+    pinMode(I1, OUTPUT);
+    pinMode(I2, OUTPUT);
+    pinMode(I3, OUTPUT);
+    pinMode(I4, OUTPUT);
+
+    digitalWrite(E1, HIGH);
+    digitalWrite(E2, HIGH);
+
+    digitalWrite(I1, HIGH);
+    digitalWrite(I2, LOW);
+    digitalWrite(I3, HIGH);
+    digitalWrite(I4, LOW);
+
+}
+
+void loop() {
+
+    digitalWrite(E1, LOW);
+    digitalWrite(E2, LOW);
+
+    delay(200);
+
+    digitalWrite(E1, HIGH);
+    digitalWrite(E2, HIGH);
+
+    digitalWrite(I1, HIGH);
+    digitalWrite(I2, LOW);
+    digitalWrite(I3, HIGH);
+    digitalWrite(I4, LOW);
+
+    delay(10000);
+
+    // change direction
+
+    digitalWrite(E1, LOW);
+    digitalWrite(E2, LOW);
+
+    delay(200);
+
+    digitalWrite(E1, HIGH);
+    digitalWrite(E2, HIGH);
+
+    digitalWrite(I1, LOW);
+    digitalWrite(I2, HIGH);
+    digitalWrite(I3, LOW);
+    digitalWrite(I4, HIGH);
+
+    delay(10000);
+}
+
+   
+
---
+++ arduino-code/TouchSensorMotor-turnandstop/TouchSensorMotor/TouchSensorMotor.ino
@@ -0,0 +1,117 @@
+//HSR 1425CR
+
+/*  use the soundsensor for Led activate  http://www.instructables.com/id/Arduino-Sound-Sensor-with-LED/
+
+  stop continuous servo : https://arduino.stackexchange.com/questions/4076/what-is-commonly-done-to-stop-a-servo-after-reaching-desired-position
+
+servoPlayPos() , HSSR 1425 does not stop
+servoStopDetach() ,  HSSR 1425 stops after turning delay time
+
+*/
+int pos = 0;   // variable to store the servo position for servoPlayPos()
+int delayServo = 5000;
+
+#include <Servo.h>
+#include <CapacitiveSensor.h>;
+
+//servo------
+Servo myservo;  // create servo object to control a servo
+
+
+//capacitive sensor------
+CapacitiveSensor   cs_4_2 = CapacitiveSensor(4, 2);       // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired
+int treshold = 1000;//1000
+
+// pin the LED is connected to
+const int LED = 12;
+
+// Define the number of samples to keep track of.  The higher the number,
+// the more the readings will be smoothed, but the slower the output will
+// respond to the input.  Using a constant rather than a normal variable lets
+// use this value to determine the size of the readings array.
+const int numReadings = 10;
+
+int readings[numReadings];      // the readings from the analog input
+int readIndex = 0;              // the index of the current reading
+int total = 0;                  // the running total
+int average = 0;                // the average
+
+//int inputPin = A0;
+int val ; // value to send to servo
+
+
+void setup()
+{
+  Serial.begin(9600);
+
+  myservo.attach(6);  // attaches the servo on pin 9 to the servo object
+  pinMode(LED, OUTPUT);
+
+  cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);// turn off autocalibrate on channel 1 - just as an example
+
+
+  // initialize serial communication with computer:
+  Serial.begin(9600);
+  // initialize all the readings to 0:
+
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+    readings[thisReading] = 0;
+  }
+
+}
+
+void loop()
+{
+  // read from the sensor:
+  long sensor1 =  cs_4_2.capacitiveSensor(120);
+  Serial.println(sensor1);  // print sensor output
+  readings[readIndex] = sensor1;
+
+  // subtract the last reading:
+  total = total - readings[readIndex];
+
+  if (sensor1 > treshold)
+  {
+    digitalWrite(LED, HIGH);
+     myservo.attach(6);
+    servoPlayandStop();
+  }
+
+  else
+  {
+    digitalWrite(LED, LOW);
+
+  }
+  // add the reading to the total:
+  total = total + readings[readIndex];
+  // advance to the next position in the array:
+  readIndex = readIndex + 1;
+
+  // if we're at the end of the array...
+  if (readIndex >= numReadings) {
+    // ...wrap around to the beginning:
+    //Serial.println("start again");
+    readIndex = 0;
+  }
+
+  // calculate the average:
+  average = total / numReadings;
+  // send it to the computer as ASCII digits
+  Serial.println(average);
+  delay(1);        // delay in between reads for stability
+
+
+
+}
+
+void servoPlayandStop() {
+
+  // attaches the servo on pin 9 to the servo object
+  delay(15);
+  myservo.write(1);
+  // sets the servo position according to the scaled value
+  delay(delayServo);// change delay for increasng or decreasing the number of turn
+  // waits for it to get to the position
+  myservo.detach();
+ delay(1000);
+}
