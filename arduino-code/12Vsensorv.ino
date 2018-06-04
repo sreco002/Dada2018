13f99188c6fb6f9cdab515f9e54aa7470ba2905e
sreco002 <sreco002@gmail.com>
2018-06-04 21:05:54 +0100

    change comment about val = value to send

--- arduino-code/12Vsensorv.ino
+++ arduino-code/12Vsensorv.ino
@@ -25,7 +25,7 @@ int readings[numReadings];      // the readings from the analog input
 int readIndex = 0;              // the index of the current reading
 int total = 0;                  // the running total
 int average = 0;                // the average
-int val ; // value to send to servo
+int val ; // value to send
 
 
 
