3819d74931a3a1d267a151b0137482dd4e2086d9
sreco002 <sreco002@gmail.com>
2018-06-05 23:54:27 +0100

    symlink addition and Dada-Api new folder

--- 0-ChatBot/node2/bot.js
+++ 0-ChatBot/node2/bot.js
@@ -1,5 +1,11 @@
 /* Shiffmann Twitter APi tuto https://www.youtube.com/watch?v=7-nX3YOC4OA */
 console.log("bot.js running");
+
+var fs = require ('fs');
+// var dataFile = fs.readFileSync('words.json');
+// var words = JSON.parse(dataFile);
+// console.log(words);
+
 var Twit = require('twit');
 //https://github.com/ttezel/twit
 var config = require ('./config');
@@ -8,19 +14,24 @@ var T = new Twit(config);
 //
 //  search twitter for all tweets containing the word 'banana' since July 11, 2011
 //
-var params ={ q: 'matiere', 
-              lang: "fr",
-             count: 8
-            }
+var params ={ q: 'computational art',
+              lang: "en",
+               count: 1
+           }
 
 T.get('search/tweets', params, gotData);
 
 function gotData(err, data, response) {
     var tweets = data.statuses;
     for (var i= 0; i< tweets.length; i++){
-    
+
+      // fs.writeFile('words.json',tweets[i].text, finished);
+      // function finished(err){
+      //   console.log('all set.');
+    //}
+
+
   console.log(tweets[i].text);
-    }
+}
   //  console.log(data);
 }
-
