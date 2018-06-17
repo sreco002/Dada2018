53fa53b5966c121a80a0e31c018d63a9503c34c2
sreco002 <sreco002@gmail.com>
2018-06-17 19:04:20 +0100

    dadaSketch and dadaSketchServer reading value from Arduino and drawing accordingly API

--- Dada-Api/T1KW/dadasketch.js
+++ Dada-Api/T1KW/dadasketch.js
@@ -7,23 +7,32 @@ function setup(){
 
   createCanvas(240,640);
   background(65);
-
+/*connecting to the server via sockets and reading message with the label=========*/
   socket = io.connect('http://localhost:3000/');//connect to the server at the address, the server is sharing infos
 //sharing
-  socket.on(label,newDrawing);// receive a message label with data from the server
-  //including data from the server
-  //and use it in the function (message, function) ie (label,newDrawing)
+//including data from the server
+//and use it in the function (message, function) ie (label,newDrawing)
 
+  socket.on(label,newDrawing);// open the channel label with data from the server
+  socket.on('listening',newSensorMsg);//another possible message/channel , use data in newSensorMsg
 }
 
+//what to do with the messages received from the server on channels label and listening
 function newDrawing(data){ //function getting the data (modified or not) from the server
   noStroke();
   fill(255,0,0);//red
-  ellipse(data.x,data.y,data.z,36); // get the new data (shared) on the server
+  ellipse(data.x,data.y,data.z,36); // use the new data (shared) on the server
+}
+
+function newSensorMsg(activate){// receiving the status of the sensor from server and display it in the browser
+if(activate== true){fill(255,0,0);}
+else if (activate == false){fill(0,255,0);}
+  noStroke();
+  triangle(18,240,18,360,240,18); // use the new data (shared) on the server
 
 }
 
-
+/*what is happening in my browser =====================================*/
 function mouseDragged(){
 console.log('Sending: '+ mouseX + ','+ mouseY);
 // define the javascript object with data in it, content of the message I want to share shared to the clients
@@ -36,5 +45,8 @@ console.log('Sending: '+ mouseX + ','+ mouseY);
   noStroke();
   fill(255);
   ellipse(mouseX,mouseY,18,18);
+
+  var activate= true;
+  socket.emit('listening',activate);
 
 }
---
+++ Dada-Api/dadaSketchserver.js
@@ -0,0 +1,103 @@
+//launch a server to host sockets from diffferent "computers" https://www.youtube.com/watch?v=bjULmG8fqc8om/watch?v=2hhEOGXcCvg
+//https://socket.io/get-started/chat/
+//index.html is the socket client ,
+// goes with index.html/talk.js from T1KW website
+
+/* client is the place where can other app /people could look at the files we want to share
+link with this website where database is listen to the request to the "website" on the port and get the output
+*/
+
+//DECLARATIONS
+//set up variables
+
+var client = 'T1KW';
+var port = '3000'
+var label = 'distance';
+var iptr = 0;
+var receivedData ="";
+
+//using libraries
+var express = require('express');
+var app = express();
+app.use(express.static(client));// this is the place where can other app /people could look at the files I want toea sharelink with this website where database is listen to the request to the "website" on the port and get the output
+
+var server = app.listen(port);// listen on that port
+var fs =require('fs');
+
+var socket = require('socket.io');
+var io = socket(server);
+
+
+//Arduino=====================================
+/* eslint-disable node/no-missing-require */
+'use strict';
+
+// Use a Readline parser
+
+var serialPort = require('serialport');
+var parsers = serialPort.parsers;
+
+// Use a `\r\n` as a line terminator
+var parser = new parsers.Readline({
+  delimiter: '\r\n'
+});
+
+var serialDevicePort = new serialPort('/dev/cu.usbmodem1421', {
+  baudRate: 9600
+});
+
+
+//----------read data from the sensor on Arduino
+ serialDevicePort.on('open', () => console.log('Port open'));
+ serialDevicePort.pipe(parser);
+
+ //parser.on('data', console.log);//print on console , what is received
+ // put the parser into an array of receivedData and translate into string
+ parser.on('data',function(data){
+   receivedData =data.toString();
+   //console.log(receivedData);
+   // checking that we can manipulate what we received
+
+   });
+
+
+
+//-------------
+
+
+
+var message = "we are the Dada Data listening"; // data goes there
+
+console.log(message+ " to "+client +" with the label :  " +label)// which website is the client
+//EVENT
+io.sockets.on('connection', newConnection);
+
+
+//FUNCTIONS
+function newConnection(socket){//called by io.sockets.on('connection', newConnection)
+
+  console.log('new connection:' + socket.id);//when there is a new connection
+  socket.on(label, drawingMsg);// if there is a message called 'label' trigger this function 'mouseMsg'link between label and function
+  socket.on('listening',sensorMsg);
+
+    function drawingMsg(data){
+    if (receivedData == "Hello") {data.z = 8;}
+    if (receivedData == "0") {data.z = 1;}
+   //console.log(receivedData);// add the received Data from Arduino to the new message
+    socket.broadcast.emit(label,data);// send back out emit the data received with this label , sent by the client website
+    //io.sockets.emit('brush',data); // emit to everyone io.sockets including the sender
+  // console.log(data);// display the data on console
+  //  console.log(message + " number "+ iptr );
+    }
+
+    function sensorMsg(data){
+    if (receivedData == "Hello") {data =true;}
+    if (receivedData == "0") {data =false;}
+  //  console.log(receivedData);// add the received Data from Arduino to the new message
+  console.log(data);// add the received Data from Arduino to the new message
+
+    io.sockets.emit('listening',data);// send back out emit the data received with this label , sent by the client website
+
+    }
+
+}
